cda258f21edd566e9c3558a69450150e879acd84
dlh <d_hoke@hotmail.com>
2020-09-21 14:01:52 -0400

    enable feature via cmake configure that conditionally excluded certain files but then unconditionally referenced items within those files that were being excluded

--- build_poppler.sh
+++ build_poppler.sh
@@ -3,7 +3,9 @@ sudo -E apt-get update
 sudo -E apt-get install -y libtool pkg-config gettext fontconfig libfontconfig1-dev cmake libzip-dev libjpeg-dev
 git clone --branch poppler-0.62.0 --depth 1 https://anongit.freedesktop.org/git/poppler/poppler.git poppler_src 
 cd poppler_src/
-cmake -DENABLE_SPLASH=OFF -DENABLE_UTILS=OFF -DENABLE_LIBOPENJPEG=none .
+#cmake -DENABLE_SPLASH=OFF -DENABLE_UTILS=OFF -DENABLE_LIBOPENJPEG=none .
+#class unconditionally tries to ref SplashOutputDev(.h) but conditionally included it with prev. cmd
+cmake                      -DENABLE_UTILS=OFF -DENABLE_LIBOPENJPEG=none .
 make
 cp libpoppler.so.?? ../pdfparser/
 cp cpp/libpoppler-cpp.so.? ../pdfparser
